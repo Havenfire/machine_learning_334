47e1e01061fcbc6e69c50b7242bbf8a082d7bb03
Havenfire <bsliu24@gmail.com>
2023-11-14 23:46:14 -0500

    q1.py complete

--- hw5/q1.py
+++ hw5/q1.py
@@ -3,8 +3,9 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_curve, auc, RocCurveDisplay
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 def normalize_feat(xTrain, xTest):
     scaler = StandardScaler()
@@ -15,18 +16,28 @@ def normalize_feat(xTrain, xTest):
     return norm_xTrain, norm_xTest
 
 def unreg_log(xTrain, yTrain, xTest, yTest):
-    model = LogisticRegression(C= 1e10)
+    model = LogisticRegression(C=1e10)
 
     model.fit(xTrain, yTrain)
 
-    y_probs = model.predict(xTest)
+    # returns probability of yes
+    y_probs = model.predict_proba(xTest)[:, 1]
 
     fpr, tpr, _ = roc_curve(yTest, y_probs)
     return fpr, tpr, auc(fpr, tpr)
 
 def run_pca(xTrain, xTest):
 
-    pca = PCA(n_components = 9)
+    pca = PCA(n_components=None)
+    pca.fit(xTrain)
+    cumulative_variance_summation = np.cumsum(pca.explained_variance_ratio_)
+
+    for index, val in enumerate(cumulative_variance_summation):
+        if val > .95:
+            num_components = index + 1
+            break
+
+    pca = PCA(n_components=num_components)
 
     t_xTrain = pca.fit_transform(xTrain)
     t_xTest = pca.transform(xTest)
@@ -55,9 +66,21 @@ def main():
     yTest = yTest.to_numpy().ravel()
 
     norm_xTrain, norm_xTest = normalize_feat(xTrain, xTest)
+    pca_xTrain, pca_xTest, pca_components = run_pca(norm_xTrain, norm_xTest)
 
-    unreg_log(xTrain, yTrain, xTest, yTest)
-    TxTrain, TxTest, PCA_C = run_pca(norm_xTrain, norm_xTest)
+    fpr, tpr, auc_val = unreg_log(norm_xTrain, yTrain, norm_xTest, yTest)
+    fpr_pca, tpr_pca, auc_val_pca = unreg_log(pca_xTrain, yTrain, pca_xTest, yTest)
+    print(auc_val, auc_val_pca)
 
+    plt.plot([0,1],[0,1], 'k--')
+    plt.plot(fpr, tpr, label= "Normalized Logisitic Regression")
+    plt.plot(fpr_pca, tpr_pca, label= "PCA logistic regression")
+    plt.legend()
+    plt.axis('square')
+
+    plt.xlabel("FPR")
+    plt.ylabel("TPR")
+    plt.title('ROC')
+    plt.show()
 if __name__ == "__main__":
     main()
